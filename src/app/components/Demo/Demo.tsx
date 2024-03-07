18face6b015ba6ffbb273a947659a2040ea6df4d
Jordan SABLON <jordan.sablon@datalegaldrive.com>
2024-03-07 17:49:34 +0100

    Adding an input field to modify our state variable

--- src/app/components/Demo/Demo.tsx
+++ src/app/components/Demo/Demo.tsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 export const Demo = () => {
     const [name, setName] = useState("John DOE")
-    
-    return <>Hello {name}, this is our first component and we are using state variable!</>;
+
+    return <>
+        Hello {name}, this is our first component and we are using state variable!
+        <br />
+        <input type="text" defaultValue={name} onChange={(e) => setName(e.target.value)} />
+    </>;
 };
