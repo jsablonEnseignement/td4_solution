720a046cef867fb6d2cd87fd0c1ae3b3fa4be555
Jordan SABLON <jordan.sablon@datalegaldrive.com>
2024-03-07 17:52:47 +0100

    Pass name variable to the Hello component

--- src/app/components/Demo/Demo.tsx
+++ src/app/components/Demo/Demo.tsx
@@ -5,7 +5,7 @@ export const Demo = () => {
     const [name, setName] = useState("John DOE")
 
     return <>
-        <Hello />
+        <Hello name={name} />
         <br />
         <input type="text" defaultValue={name} onChange={(e) => setName(e.target.value)} />
     </>;
--- src/app/components/Demo/Hello.tsx
+++ src/app/components/Demo/Hello.tsx
@@ -1,4 +1,8 @@
 
-export const Hello = () => {
+type HelloProps = {
+    name: string;
+}
+
+export const Hello = ({ name } : HelloProps) => {
     return <>Hello {name}, this is our first component and we are using state variable!</>;
 };
