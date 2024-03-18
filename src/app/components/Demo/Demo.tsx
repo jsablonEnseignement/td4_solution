6cb97faa9729cfa6482133cda47329f2c38e56e9
Jordan SABLON <jordan.sablon@datalegaldrive.com>
2024-03-18 08:34:35 +0100

    Ajout des useEffect

--- src/app/components/Demo/Demo.tsx
+++ src/app/components/Demo/Demo.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Hello } from "./Hello";
 
 export const Demo = () => {
-    const [name, setName] = useState("John DOE")
+    const [name, setName] = useState("John DOE");
+
+    useEffect(() => {
+        console.log("Ce texte est affiché au chargement du composant !");
+    }, []);
+
+    useEffect(() => {
+        console.log(`La variable name contient ${name.length} caractères !`);
+    }, [name]);
+        
 
     return <>
         {name ? <Hello name={name} /> : "Veuillez renseigner un nom d'utilisateur"}
         <br />
         <input type="text" defaultValue={name} onChange={(e) => setName(e.target.value)} />
-    </>;
+    </>
 };
