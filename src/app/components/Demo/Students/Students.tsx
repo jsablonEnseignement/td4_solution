6cc8078e4a9b558ba4460737ea66e8c2576dfaca
Jordan SABLON <jordan.sablon@datalegaldrive.com>
2024-03-18 08:37:48 +0100

    Fetch students from backend

--- src/app/components/Demo/Students/Students.tsx
+++ src/app/components/Demo/Students/Students.tsx
@@ -1,4 +1,11 @@
+import axios from "axios";
+
 export const Students = () => {
-      return <>Hello!</>
+    const fetchStudents = async () => {
+        const { data } = await axios.get(`http://localhost:5000/students`);
+        console.log(data);
     };
+        
+    return <button onClick={() => fetchStudents()}>Récupérer les étudiants</button>;
+};
     
